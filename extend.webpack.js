67f1698957c015335fd55aa4e0134b1bbfeb6c13
Morgan Pizzini <morgan.pizzini@hotmail.com>
2019-09-20 07:33:57 +0200

    build guess script

--- extend.webpack.js
+++ extend.webpack.js
@@ -1,6 +1,10 @@
 const { GuessPlugin } = require('guess-webpack');
 const { parseRoutes } = require('guess-parser');
 
+// const { parseAngularRoutes } = require('guess-parser');
+
+// parseAngularRoutes('PATH_TO_TS_CONFIG', ['./src/app/app-routing.module.ts']);
+
 module.exports = {
   plugins: [
     new GuessPlugin({
